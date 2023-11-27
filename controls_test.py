3858c541d564212a6a11fa49d1ed944c723e99f1
baycat <anyssa.cast@gmail.com>
2023-11-27 16:02:13 -0600

    comments for speed control

--- controls_test.py
+++ controls_test.py
@@ -1,32 +1,56 @@
+# import statments
 import math
 import time
 
+# function to write a new initial value to the gpio pin
 def set_speed(speed):
+    # access pin initial values
     with open('/dev/bone/pwm/1/a/duty_cycle', 'w') as filetowrite:
+        # write desired speeds to gpio values
         filetowrite.write(str(speed))
 
+# function to drive motor at a slow speed
 def steady_speed():
+    # set speed value to give 8.1% to drive motor
     steady_speed = 1620000
+    # print speed setting
     print("speed: ", steady_speed)
+    # print 'going' intention
     print("going")
+    # write the new speed value to the initial gpio pin setting to drive motor
     set_speed(steady_speed)
 
+# function to drive motor at a faster speed
 def faster_speed():
+    # set speed value to give 8.175% to drive motor
     faster_speed = 1635000
+    # print speed setting
     print("speed: ", faster_speed)
+    # print 'going faster' intention
     print("going faster")
+    # write the new speed value to the initial gpio pin setting to drive motor
     set_speed(faster_speed)
 
+# function to drive motor at a slower speed
 def crawling_speed():
+    # set speed value to give 8.05% to drive motor
     slow_speed = 1610000
+    # print speed setting
     print("speed: ", slow_speed)
+    # print 'going slower' intention
     print("going slower")
+    # write the new speed value to the initial gpio pin setting to drive motor
     set_speed(slow_speed)
 
+# function to drive motor at a stop speed
 def stop():
+    # set speed value to give 8% to drive motor
     stop_speed = 1600000
+    # print speed setting
     print("speed: ", stop_speed)
+    # print 'going stop' intention
     print("going stop")
+    # write the new speed value to the initial gpio pin setting to drive motor
     set_speed(stop_speed)
 
 
