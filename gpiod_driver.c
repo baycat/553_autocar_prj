b2df252148b9221821074a73a625f1b6f5b45939
Dingding Ye <dy32@rice.edu>
2023-12-04 15:37:10 -0600

    driver updated with timing differences and corrected to one struct pointer

--- gpiod_driver.c
+++ gpiod_driver.c
@@ -8,28 +8,39 @@
 #include <linux/interrupt.h>
 
 // LED and button pointer declarations
-struct gpio_desc *led_gpios;
+//struct gpio_desc *led_gpios;
 struct gpio_desc *button_gpios;
 
-/** variable contains pin number o interrupt controller to which GPIO 6 is mapped
-to */
+/** variable contains pin number to interrupt controller: assigned to P8_03 on BBAI64 */
 unsigned int irq_number;
+
+// Time variables
+ktime_t currTime;
+ktime_t prevTime;
+int diff; 
+
+// print timing difference to a separate file
+module_param(diff, int, S_IRUGO);
 
 /**
 * @brief Interrupt service routine is called, when interrupt is triggered
 */
 static irq_handler_t gpio_irq_handler(unsigned int irq, void *dev_id, struct
 pt_regs *regs) {
-	// printing when button is pressed
-	printk("handling... (button pressed!)\n");
+	// calculate timing difference between triggers for ESC
+	currTime = ktime_get();
+	int temp_time = ktime_to_ns(currTime - prevTime) / 1000000;
 
-	// get current value of LED and toggle to opposite value
-	int led_val;
-	led_val = gpiod_get_value(led_gpios);
-	gpiod_set_value(led_gpios, !led_val);
+	// reject timing below 1ms
+	if (temp > 1) {
+		diff = temp;
+	}
 
-	//print value of LED for sanity checking and exit
-	printk("led_val %d\n", !led_val);
+	// observe time difference and update time
+	printk("timing difference of: %d\n", diff); 
+	prevTime = currTime;
+
+	// finish handling 
 	return (irq_handler_t) IRQ_HANDLED;
 }
 
@@ -41,37 +52,38 @@ static int led_probe(struct platform_device *pdev) {
 	// printing when module is inserted
 	printk("gpio_irq: Loading module... ");
 
-	//get button and LED struct pointers
+	// get struct pointers
 	button_gpios = devm_gpiod_get(&pdev->dev, "button", GPIOD_IN);
-	led_gpios = devm_gpiod_get(&pdev->dev, "led", GPIOD_OUT_LOW);
 
-	/* Setup the interrupt to correspond to the button*/
+	// set up the interrupt to correspond to the encoder
 	irq_number = gpiod_to_irq(button_gpios);
 
 	// interrupt error, also interrupt triggered on falling edge
-	if(request_irq(irq_number, (irq_handler_t) gpio_irq_handler,
-
-IRQF_TRIGGER_FALLING, "button_irq", NULL) != 0){
-	printk("Error!\nCan not request interrupt nr.: %d\n", irq_number);
-	gpio_free(led_gpios);
-	return -1; // exit with error
+	if(request_irq(irq_number, (irq_handler_t) gpio_irq_handler, IRQF_TRIGGER_FALLING, "button_irq", NULL) != 0){
+		printk("Error!\nCan not request interrupt nr.: %d\n", irq_number);
+		return -1; // exit with error
 	}
 
+	// set up debouncing
+	printk("debouncing...\n");
+	gpiod_set_debounce(button_gpios, 1000000); // time in microseconds
+
 	// exit peacefully
+	printk("done\n");
 	return 0;
 }
 
 // remove function
 static int led_remove(struct platform_device *pdev)
 {
-	/*Free the irq and print a message */
+	// free the irq and print a message
 	free_irq(irq_number, NULL);
 	printk("it's removeddddd\n");
 	return 0;
 }
 
 static struct of_device_id matchy_match[] = {
-	{ .compatible= "opticalencoder", },
+	{ .compatible= "opticalencoder", }, // our driver is named opticalencoder
 	{/* leave alone - keep this here (end node) */},
 };
 
