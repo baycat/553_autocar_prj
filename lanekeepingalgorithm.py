84f8a585986de4464da9bf3c2faab13a5b83c42a
Dingding Ye <dy32@rice.edu>
2023-12-07 10:30:33 -0600

    slightly faster

--- lanekeepingalgorithm.py
+++ lanekeepingalgorithm.py
@@ -141,11 +141,11 @@ def go():
     """
     global current_speed
 
-    current_speed = 1627500
+    current_speed = 1628500
 
     #Write to move the car
     with open('/dev/bone/pwm/1/a/duty_cycle', 'w') as filetowrite:
-        filetowrite.write('1627500')
+        filetowrite.write('1628500')
 
 def boost():
 
