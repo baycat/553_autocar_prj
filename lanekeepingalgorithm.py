c22e5f7f9c75e19ccf30247ead4d88f75adc9c65
Dingding Ye <dy32@rice.edu>
2023-12-07 11:16:05 -0600

    check for second stop less often

--- lanekeepingalgorithm.py
+++ lanekeepingalgorithm.py
@@ -470,13 +470,14 @@ while counter < max_ticks:
                 # add a delay to calling go faster
                 go_faster_tick = counter + go_faster_tick_delay
                 print("first stop finished!")
+                go()
                 boost()
                 
 
 
         # check for the second stop sign
         elif passedFirstStopSign and counter > secondStopSignTick:
-
+            go()
             boost()
             print(secondStopSignTick)
             print (counter)
